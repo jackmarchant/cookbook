9999d9d90de355bcb3482669925d21daecf4de0f
Jack Marchant <jack@jackmarchant.com>
2019-11-25 23:15:16 +1100

    Update settings with ClearDB env

--- app/settings.php
+++ app/settings.php
@@ -3,6 +3,23 @@
  * Application settings and configuration
  */
 
+if (getenv('CLEARDB_DATABASE_URL')) {
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+
+    return [
+        'database' => [
+            'host' => $cleardb_server,
+            'username' => $cleardb_username,
+            'password' => $cleardb_password,
+            'dbname' => $cleardb_db,
+        ]
+    ];
+}
+
 return [
     'database' => [
         'host' => getenv('MYSQL_HOST'),
